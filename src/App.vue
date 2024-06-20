a812f8b4549dd33cd4a06cbff3bd248685add72b
Samanja Cartagena <c.samanja09@Pabs.local>
2024-06-20 10:00:49 -0400

    side components emits

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <sides @someevent='trigger'></sides>
+    <p>{{this.message}}</p>
+  </div>
+</template>
+
+<script>
+import Sides from './Sides.vue'
+export default {
+  emits:['someevent'],
+  components:{Sides},
+  data(){
+    return{
+      message:''
+    }
+  },
+  methods:{
+    trigger(msg){
+      this.message=msg
+    }
+  }
+  
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/Sides.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+   <h1>Hey</h1>
+   <button @click='send'>Send</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+        message:'coconut'
+    }
+  },
+ 
+ methods:{
+    send(){
+        this.$emit('someevent',this.message)
+    }
+ }
+}
+</script>
+
+<style>
+
+</style>
